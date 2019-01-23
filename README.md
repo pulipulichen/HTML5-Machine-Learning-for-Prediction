da88cfbb46e30428316e153b3ab6cbb7436b4c99
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-01-23 16:30:01 +0800

    0123 隨機森林..有點微妙，不過還可以，就先這樣吧

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HTML5-Machine-Learning-for-Prediction
 A HTML widget for prediction with machine learning algorithm
+
+- 看來crossvaildation可能要自己寫
--- script/main.js
+++ script/main.js
@@ -9,11 +9,62 @@ const output = net.run({ r: 0.03, g: 0.7, b: 0.5 });  // { white: 0.99, black: 0
 log(output)
 log(output['black']+ output['white'])
 */
+
+/*
 MLR = ML.MultivariateLinearRegression
 
 const x = [[0, 0], [1, 2], [2, 3], [3, 4]];
 // Y0 = X0 * 2, Y1 = X1 * 2, Y2 = X0 + X1
-const y = [[0, 0, 0], [2, 4, 3], [4, 6, 5], [6, 8, 7]];
+const y = [[0], [2], [4], [6]];
 const mlr = new MLR(x, y);
-console.log(mlr.predict([3, 3]));
-log(mlr.predict([1, 2]))
+log(mlr.predict([1, 3]))
+*/
+
+RFRegression = ML.RandomForestRegression
+var dataset = [
+  [73, 80, 75, 152],
+  [93, 88, 93, 185],
+  [89, 91, 90, 180],
+  [96, 98, 100, 196],
+  [73, 66, 70, 142],
+  [53, 46, 55, 101],
+  [69, 74, 77, 149],
+  [47, 56, 60, 115],
+  [87, 79, 90, 175],
+  [79, 70, 88, 164],
+  [69, 70, 73, 141],
+  [70, 65, 74, 141],
+  [93, 95, 91, 184],
+  [79, 80, 73, 152],
+  [70, 73, 78, 148],
+  [93, 89, 96, 192],
+  [78, 75, 68, 147],
+  [81, 90, 93, 183],
+  [88, 92, 86, 177],
+  [78, 83, 77, 159],
+  [82, 86, 90, 177],
+  [86, 82, 89, 175],
+  [78, 83, 85, 175],
+  [76, 83, 71, 149],
+  [96, 93, 95, 192]
+];
+
+var trainingSet = new Array(dataset.length);
+var predictions = new Array(dataset.length);
+
+for (var i = 0; i < dataset.length; ++i) {
+  trainingSet[i] = dataset[i].slice(0, 3);
+  predictions[i] = dataset[i][3];
+}
+
+var options = {
+  seed: 3,
+  maxFeatures: 2,
+  replacement: false,
+  nEstimators: 200
+};
+
+var regression = new RFRegression(options);
+regression.train(trainingSet, predictions);
+var result = regression.predict([[70, 93, 73]]);
+log(result)
